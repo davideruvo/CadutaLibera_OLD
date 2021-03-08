305882071413c0142adf3d4c33978750dad74db0
davideruvo <79589610+davideruvo@users.noreply.github.com>
2021-03-08 16:36:26 +0100

    Manager

--- js/manager.js
+++ js/manager.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
     init();
     function init() {
-        loadQuestions();
-        $('.controls').append($('<button>').tooltip({ content: 'Aggiorna dati', type: constants.messageType.info, left: true }).addClass('btn-apply').addClass('btn btn-circle shadow')
-            .append($('<i>').addClass('bi bi-upload'))
-            .on('click', updateData));
+        createLogo();
+        $('.btn-load').tooltip({ content: 'Leggi dati', type: constants.messageType.info, left: true })
+            .off('click').on('click', loadData);
+        $('.btn-update').tooltip({ content: 'Aggiorna dati', type: constants.messageType.info, left: true })
+            .off('click').on('click', updateData);
     }
-    function loadQuestions() {
+    function loadData() {
         ajaxCall('/api/getQuestions', 'get',
             {
                 success_callback: function (result) {
